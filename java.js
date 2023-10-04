be68bf64ca22d099be3edd396265704ef3409641
ahhuann <huan99k1@gmail.com>
2023-10-04 22:17:38 +0700

    add toantu.html

---
+++ java.js
@@ -0,0 +1,10 @@
+let inputWidth = prompt("Enter the width");
+let inputHeight = prompt("Enter the height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Enter the width: " + "<br>" + + inputWidth);
+document.write("<br>");
+document.write("Enter the height: " + "<br>" + + inputHeight);
+document.write("<br>"); 10
+document.write("The area is: " + "<br>" + + area);
